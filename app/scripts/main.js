348d1d189b72818369860c75344189c8fcedb423
blitnee <brittneelc@gmail.com>
2017-02-15 13:58:50 -0500

    Refine WebWorker
    Addition error function and fix bug in imageDecoderWorker, Remove HTML
    scripts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -48,6 +48,8 @@
 
     var client = new QRClient();
 
+    var imageDecoderWorker = new Worker('scripts/jsqrcode/qrworker.js');
+
     var self = this;
 
     this.currentUrl = undefined;
@@ -56,12 +58,24 @@
     this.detectQRCode = function(imageData, callback) {
       callback = callback || function() {};
 
-      client.decode(imageData, function(result) {
-        if(result !== undefined) {
-          self.currentUrl = result;
+      imageDecoderWorker.postMessage(imageData);
+
+      imageDecoderWorker.onmessage = function(result) {
+        var url = result.data;
+        if (url !== undefined) {
+          self.currentUrl = url;
         }
-        callback(result);
-      });
+        callback(url);
+      };
+
+      imageDecoderWorker.onerror = function(error) {
+        function WorkerException(message) {
+          this.name = "WorkerException";
+          this.message = message;
+        };
+        throw new WorkerException('Decoder error');
+        callback(undefined);
+      };
     };
 
     this.showDialog = function(url) {
